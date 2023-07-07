0ba25ecd957cc7dea91dae5c4898fd230061f11a
Kat Nix <deutschkat29@gmail.com>
2023-07-07 18:55:02 -0500

    BIIIIIG FIX

--- src/main/java/com/example/CustomerWebsite/security/SecurityConfig.java
+++ src/main/java/com/example/CustomerWebsite/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -13,6 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
